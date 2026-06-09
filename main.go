ce8ed914fa0544998a72b0658b4547693de37d49
alecmstockman <alecmstockman@gmail.com>
2026-06-09 18:05:12 -0500

    comment cleanup

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"net/http"
 )
 
-// var db *sql.DB
 var messages []string
 
 func main() {
@@ -38,54 +37,3 @@ func main() {
 	log.Println("Server running on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
-// func homeHandler(w http.ResponseWriter, r *http.Request) {
-// 	messages, err := handlers.messagesTableGetAllMessages()
-// 	if err != nil {
-// 		fmt.Println("Unable to get messages from db")
-// 	}
-
-// 	tmpl.Execute(w, messages)
-// }
-
-// func sendHandler(w http.ResponseWriter, r *http.Request) {
-// 	message := r.FormValue("message")
-
-// 	err := handlers.messagesTableInsertMessage(message)
-// 	if err != nil {
-// 		http.Error(w, "Could not save message", http.StatusInternalServerError)
-// 		return
-// 	}
-
-// 	html := fmt.Sprintf(
-// 		"<li>%s</li>",
-// 		message,
-// 	)
-
-// 	w.Write([]byte(html))
-// }
-
-// func deleteHandler(w http.ResponseWriter, r *http.Request) {
-// 	err := messagesTableDeleteAll()
-// 	if err != nil {
-// 		http.Error(w, "Could not delete messages", http.StatusInternalServerError)
-// 		return
-// 	}
-
-// 	w.Write([]byte(""))
-// 	return
-// }
-
-// func messagesHandler(w http.ResponseWriter, r *http.Request) {
-// 	messages, err := messagesTableGetAllMessages()
-
-// 	if err != nil {
-// 		http.Error(w, "Could not fetch latest messages", http.StatusInternalServerError)
-// 		return
-// 	}
-
-// 	for _, msg := range messages {
-// 		html := fmt.Sprintf("<li>%s</li>", msg)
-// 		w.Write([]byte(html))
-// 	}
-// }
