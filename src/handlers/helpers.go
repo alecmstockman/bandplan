178e8583f2fd6f9e04fbc148d1b5d09d4a47344d
alecmstockman <alecmstockman@gmail.com>
2026-06-12 16:33:30 -0500

    created get authenticated user helper

--- src/handlers/helpers.go
+++ src/handlers/helpers.go
@@ -3,7 +3,12 @@ package handlers
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"fmt"
+	"net/http"
 	"time"
+
+	"bandplan/src/database"
+	"bandplan/src/models"
 )
 
 func GenerateSessionToken() (string, error) {
@@ -18,4 +23,21 @@ func GenerateSessionToken() (string, error) {
 func GenerateSessionExpiration() time.Time {
 	expiration := time.Now().Add(1 * time.Hour)
 	return expiration
+}
+
+func GetAuthenticatedUser(r *http.Request) (models.User, error) {
+	cookie, err := r.Cookie("session_token")
+	if err != nil {
+		fmt.Println("Error getting authenticated user: ", err)
+		return models.User{}, err
+	}
+	fmt.Println("Get Authenticated User cookie: ", cookie)
+
+	user, err := database.SessionsTableGetUserByToken(cookie.Value)
+	if err != nil {
+		fmt.Println("Unable to get user by token: ", err)
+		return models.User{}, nil
+	}
+
+	return user, nil
 }
