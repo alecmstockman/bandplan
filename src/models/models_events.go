3b49ef759c4f1138253d7d9e942613ca4ca6b875
alecmstockman <alecmstockman@gmail.com>
2026-08-18 14:31:25 -0500

    created event struct

---
+++ src/models/models_events.go
@@ -0,0 +1,34 @@
+package models
+
+import "time"
+
+type Event struct {
+	ID      int
+	EventID string
+	BandID  string
+
+	Title       string
+	Slug        string
+	Description string
+
+	EventType string
+
+	StartTime time.Time
+	EndTime   time.Time
+	Timezone  string
+
+	VenueName string
+	Address   string
+	City      string
+	State     string
+	ZipCode   string
+
+	SetlistID string
+
+	Notes string
+
+	CreatedAt time.Time
+	CreatedBy string
+	UpdatedAt time.Time
+	UpdatedBy string
+}
