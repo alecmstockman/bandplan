602f2b3dadc4c06d79f2150c863c3e516c1264bf
alecmstockman <alecmstockman@gmail.com>
2026-09-21 20:41:55 -0500

    creted user_registrations table

--- sql/schema/20260921232740_events_user_registration.sql
+++ sql/schema/20260921232740_events_user_registration.sql
@@ -1,5 +1,5 @@
 -- +goose Up
-CREATE TABLE user_registration (
+CREATE TABLE user_registrations (
     id SERIAL PRIMARY KEY,
     user_registration_id TEXT NOT NULL UNIQUE,
     access_code_hash TEXT UNIQUE,
@@ -7,19 +7,26 @@ CREATE TABLE user_registration (
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     display_name TEXT NOT NULL,
-    slug TEXT UNIQUE,
 
-    timezone TEXT,
+    band_id TEXT REFERENCES bands(band_id) ON DELETE SET NULL,
+    timezone TEXT NOT NULL,
 
-    email TEXT NOT NULL UNIQUE,
+    email TEXT,
     email_verified BOOLEAN NOT NULL DEFAULT FALSE,
 
-    password_hash TEXT NOT NULL,
-    is_admin BOOLEAN NOT NULL DEFAULT FALSE,
+    password_hash TEXT,
 
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP;
-)
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    expires_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP + INTERVAL '24 hours',
+
+    CHECK (NOT email_verified OR email IS NOT NULL),
+    CHECK (password_hash IS NULL OR password_hash <> '')
+);
+
+CREATE UNIQUE INDEX idx_user_registrations_email
+ON user_registrations (LOWER(email))
+WHERE email IS NOT NULL;
 
 -- +goose Down
 DROP TABLE user_registration;
